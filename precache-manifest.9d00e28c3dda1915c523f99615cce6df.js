173bf531ff65a4ba40ace35fc4d0752043b234ea
Miguel Arenas Villalobos <admin@miguelarenasvillalobos.com>
2020-04-19 09:50:14 -0500

    Updates

---
+++ precache-manifest.9d00e28c3dda1915c523f99615cce6df.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "8056ea321a4c5fda5fa1",
+    "url": "/PlatziBadges/static/css/main.b327a6c4.chunk.css"
+  },
+  {
+    "revision": "8056ea321a4c5fda5fa1",
+    "url": "/PlatziBadges/static/js/main.8056ea32.chunk.js"
+  },
+  {
+    "revision": "a53ed276095f699ff651",
+    "url": "/PlatziBadges/static/js/runtime~main.a53ed276.js"
+  },
+  {
+    "revision": "e6799b2b12faa28da446",
+    "url": "/PlatziBadges/static/css/2.ae820ead.chunk.css"
+  },
+  {
+    "revision": "e6799b2b12faa28da446",
+    "url": "/PlatziBadges/static/js/2.e6799b2b.chunk.js"
+  },
+  {
+    "revision": "949f0b663a26f036e092ce33cb0b1203",
+    "url": "/PlatziBadges/static/media/platziConf-logo.949f0b66.svg"
+  },
+  {
+    "revision": "dc24081b9a595a1b8d52f5ec29670df0",
+    "url": "/PlatziBadges/static/media/badge-header.dc24081b.svg"
+  },
+  {
+    "revision": "044973fd1d313ace4c7cedf78676d24f",
+    "url": "/PlatziBadges/static/media/logo.044973fd.svg"
+  },
+  {
+    "revision": "abf4ab106abb8c3c6b4291fd2a2526f4",
+    "url": "/PlatziBadges/static/media/platzi-logo.abf4ab10.svg"
+  },
+  {
+    "revision": "76c503a590633e04cb6bc3fd891ae432",
+    "url": "/PlatziBadges/static/media/conf-logo.76c503a5.svg"
+  },
+  {
+    "revision": "73ec3ffb30175fb20ed36d72741b54f7",
+    "url": "/PlatziBadges/static/media/conf.73ec3ffb.svg"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/PlatziBadges/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/PlatziBadges/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/PlatziBadges/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/PlatziBadges/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/PlatziBadges/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/PlatziBadges/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/PlatziBadges/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/PlatziBadges/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/PlatziBadges/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/PlatziBadges/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/PlatziBadges/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/PlatziBadges/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/PlatziBadges/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/PlatziBadges/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/PlatziBadges/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "c24e6dd10b9960af5f45bc6156d97448",
+    "url": "/PlatziBadges/static/media/stars.c24e6dd1.svg"
+  },
+  {
+    "revision": "5d0dbe5da8bed17345b352ac36fe78e0",
+    "url": "/PlatziBadges/index.html"
+  }
+];
